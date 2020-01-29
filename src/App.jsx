2246f60e31b40fbaab328ae516d10b0475ae67e8
Marion Douet <squalaladraw@gmail.com>
2020-01-29 17:17:33 +0100

    add list of entrees

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import FormRecipe from './components/FormRecipe'
 import ShowDessert from './components/ShowDessert'
 import ShowMeal from './components/ShowMeal'
+import ShowEntree from './components/ShowEntree'
 import './App.css';
 import HomePage from './components/Homepage';
 
@@ -11,8 +12,9 @@ function App() {
     <div className="App">
       <Route exact path="/" component={HomePage} />
       <Route path="/formrecipe" component={FormRecipe} />
-      <Route path="/dessert" component={ShowDessert} />
+      <Route path="/entree" component={ShowEntree} />
       <Route path="/meal" component={ShowMeal} />
+      <Route path="/dessert" component={ShowDessert} />
     </div>
   );
 }
---
+++ src/components/ShowEntree.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import RecipeList from './RecipeList'
+import axios from 'axios';
+
+
+class ShowEntree extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [],
+    };
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios
+    .get('/littlecooker/recipe/entree')
+      .then(res => res.data)
+      .then(data => this.setState({ recipes: data }));
+  }
+
+  render() {
+    const { recipes } = this.state;
+      return (
+        <div className="">
+          <RecipeList recipes={recipes}/>
+        </div>
+      );
+  }
+}
+
+export default ShowEntree;
