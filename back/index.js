08232cdd73a6b03a63c1bf3436cdda3f0509c5d4
Marion Douet <squalaladraw@gmail.com>
2020-01-29 14:14:41 +0100

    installation du repo

---
+++ back/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const app = express();
+const port = 4000;
+const bodyParser = require('body-parser');
+const connection = require('./conf');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.post('/littlecooker/recipe', (req, res) => {
+  const formData = req.body;
+  connection.query('INSERT INTO recipe SET ?', formData, err => {
+    if (err) {
+      res.status(500).send('Erreur');
+    } else {
+      res.status(201).send('recipe created');
+    }
+  });
+});
+
+app.post('/littlecooker/type', (req, res) => {
+  const formData = req.body;
+  connection.query('INSERT INTO type SET ?', formData, err => {
+    if (err) {
+      console.log(err)
+      res.status(500).send('Erreur');
+    } else {
+      res.status(201).send('type of recipe created');
+    }
+  });
+});
+
+
+app.listen(port, err => {
+  if (err) {
+    throw new Error('Something bad happened...');
+  }
+  console.log(`Server is listening on ${port}`);
+});
